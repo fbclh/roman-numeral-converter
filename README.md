f090e9b954f555036602d528776d16ef0c57281c
fbclh <fbclh@protonmail.com>
2020-11-28 10:09:03 -0300

    Refactor algo and edit README

--- README.md
+++ README.md
@@ -2,7 +2,27 @@
 
 freeCodeCamp JavaScript Algorithms and Data Structure project
 
-## To Do
+## Description
 
-1. Make a UI version
-1. Add screenshot
+Convert the given arabic number into a roman numeral.
+
+## Example
+
+```JavaScript
+// Roman Numeral Converter
+const convertToRoman = (num) => {
+  const arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  for (let i = 0; i < arabic.length; i++) {
+    if (num === 0 || num < 1) return '';
+    if (num >= arabic[i]) return roman[i] + convertToRoman(num - arabic[i]);
+  }
+};
+
+// Input/Output
+console.log(convertToRoman(33)); // XXIII
+console.log(convertToRoman(93)); // XCIII
+console.log(convertToRoman(777)); // DCCLXXVII
+
+```
--- script.ts
+++ script.ts
@@ -1,33 +1,15 @@
-// Convert the given arabic number into a roman numeral.
-
-function convertToRoman(num) {
+// Roman Numeral Converter
+const convertToRoman = (num) => {
   const arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  const roman = [
-    'M',
-    'CM',
-    'D',
-    'CD',
-    'C',
-    'XC',
-    'L',
-    'XL',
-    'X',
-    'IX',
-    'V',
-    'IV',
-    'I',
-  ];
+  const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
   for (let i = 0; i < arabic.length; i++) {
     if (num === 0 || num < 1) return '';
     if (num >= arabic[i]) return roman[i] + convertToRoman(num - arabic[i]);
   }
-}
+};
 
-console.log(convertToRoman(3)); // III
-console.log(convertToRoman(7)); // VII
-console.log(convertToRoman(11)); // XI
-console.log(convertToRoman(400)); // CD
-console.log(convertToRoman(1000)); // M
-
-
+// Input/Output
+console.log(convertToRoman(33)); // XXIII
+console.log(convertToRoman(93)); // XCIII
+console.log(convertToRoman(777)); // DCCLXXVII
